bea2a46d749912e06080b98c8f82f53c5ef2c6ed
user <esy_win@hotmail.com>
2022-03-15 22:01:47 +0100

    remove reboot for debug

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo add-apt-repository ppa:ondrej/php
 # update system
 sudo apt update && sudo apt upgrade
 # install git, wget, curl, openssl and software-properties-common
-sudo apt install screen wget openssl redis-server nginx mysql-server php7.3 php7.3-fpm php7.3-mysql php7.3-mbstring php7.3-imagick php7.3-xml php7.3-bcmath php7.3-intl php7.3-zip -y
+sudo apt install screen wget openssl redis-server supervisor nginx mysql-server php7.3 php7.3-fpm php7.3-mysql php7.3-mbstring php7.3-imagick php7.3-xml php7.3-bcmath php7.3-intl php7.3-zip -y
 sudo mysql_secure_installation -y
 # generate three high-difficulty passwords
 PASS_WALLET=$(openssl rand 60 | openssl base64 -A)
