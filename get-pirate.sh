d73bc9e8c3a3117b900f3c673b73773ffd6683c1
user <esy_win@hotmail.com>
2022-03-17 00:02:53 +0100

    passing arguments to shell prompts

--- get-pirate.sh
+++ get-pirate.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install build-essential pkg-config libc6-dev m4 g++-multilib autoconf libtool libncurses-dev unzip git python zlib1g-dev wget bsdmainutils automake libboost-all-dev libssl-dev libprotobuf-dev protobuf-compiler libqrencode-dev libdb++-dev ntp ntpdate nano software-properties-common curl libevent-dev libcurl4-gnutls-dev cmake clang libsodium-dev -y
+sudo apt update && sudo apt upgrade -y
+sudo apt install build-essential pkg-config libc6-dev m4 g++-multilib autoconf libtool libncurses-dev unzip git python zlib1g-dev wget bsdmainutils automake libboost-all-dev libssl-dev libprotobuf-dev protobuf-compiler libqrencode-dev libdb++-dev ntp ntpdate nano software-properties-common curl libevent-dev libcurl4-gnutls-dev cmake clang libsodium-dev -y
 cd ~
 git clone https://github.com/PirateNetwork/pirate --branch master
 cd pirate
 ./zcutil/fetch-params.sh
-./zcutil/build.sh -j$(nproc - 1)
-./pirated -bootstrap=2
+./zcutil/build.sh -j$(nproc - 2)
--- install.sh
+++ install.sh
@@ -5,15 +5,25 @@ sudo apt update
 sudo apt upgrade -y
 # install git, wget, curl, openssl and software-properties-common
 sudo apt install screen wget openssl redis-server supervisor nginx mysql-server php7.3 php7.3-fpm php7.3-mysql php7.3-mbstring php7.3-imagick php7.3-xml php7.3-bcmath php7.3-intl php7.3-zip -y
-# sudo mysql_secure_installation -y
 # generate three high-difficulty passwords
 PASS_WALLET=$(openssl rand 60 | openssl base64 -A)
 PASS_MYSQL=$(openssl rand 60 | openssl base64 -A)
 PASS_REDIS=$(openssl rand 60 | openssl base64 -A)
+# pass answers to prompts
+{ echo 'Y';
+  echo '1';
+  echo $PASS_MYSQL;
+  echo $PASS_MYSQL;
+  echo 'Y';
+  echo 'Y';
+  echo 'Y';
+  echo 'Y';
+  echo 'Y';
+}  | sudo mysql_secure_installation
+
 # replace rpcpassword & rpcuser
 sed -i "2i rpcpassword=$PASS_WALLET" /home/$USER/.komodo/PIRATE/PIRATE.conf
 sed -i "1i rpcuser=piratepay" /home/$USER/.komodo/PIRATE/PIRATE.conf
-cd ~
 # root into mysql, create database 'piratepay' grant all to piratepay
 mysql -u root -p$PASS_MYSQL << EOF
 CREATE DATABASE piratepay DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
@@ -22,7 +32,7 @@ FLUSH PRIVILEGES;
 QUIT;
 EOF
 # setting fix
-sudo sed -i /etc/php/7.3/fpm/php.ini 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g'
+# sudo sed -i /etc/php/7.3/fpm/php.ini 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g'
 # add php to system boot
 sudo systemctl restart php7.3-fpm
 # download our config file to replace default
@@ -36,33 +46,31 @@ sudo systemctl reload nginx
 sudo sed -i /etc/redis/redis.conf "s/# requirepass foobared/requirepass $PASS_REDIS/g"
 sudo systemctl restart redis.service
 cd ~
-# download composer
+# get composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
+# piratepay install
 cd /var/www
-# PiratePay install in nginx folder
 sudo git clone https://github.com/CryptocurrencyCheckout/PiratePay.git
 cd PiratePay
-sudo sed -i horizon.conf "s/user=ubuntu/user=$USER/g"
+# replace user in horizon.conf, relplace config file and reload supervisor
+sudo sed -i "6i user=$USER" horizon.conf
 sudo mv horizon.conf /etc/supervisor/conf.d/horizon.conf
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl start horizon
-# a bunch of chmod
-sudo chown -R www-data:ubuntu /var/www/PiratePay
+# batch chmod
+sudo chown -R www-data:$USER /var/www/PiratePay
 sudo find /var/www/PiratePay -type f -exec chmod 664 {} \;
 sudo find /var/www/PiratePay -type d -exec chmod 775 {} \;
 # install our stack with composer
 composer install --no-dev
 # create .env file from .env.example
-cp .env.example .env
-# replace respectives occurences 
+sudo cp .env.example .env
+# replace with our config
 sudo sed -i "5i APP_URL=localhost" .env
 sudo sed -i "10i PIRATE_PASSWORD=$PASS_WALLET" .env
 sudo sed -i "23i DB_PASSWORD=$PASS_MYSQL" .env
 sudo sed -i "32i REDIS_PASSWORD=$PASS_REDIS" .env
 # migrate database & generate encryption keys
 php artisan migrate && php artisan key:generate && php artisan passport:install --length=512 --force
-# start piratechain deamon & sync to latest block
-cd
-/home/$USER/pirate/komodod -ac_name=PIRATE -ac_supply=0 -ac_reward=25600000000 -ac_halving=77777 -ac_private=1 &
