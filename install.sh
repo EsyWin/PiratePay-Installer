0108b0fd4e6a9afc509e1d7e341ac0388ed2880b
user <esy_win@hotmail.com>
2022-03-16 20:10:18 +0100

    fix

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt update 
 sudo apt upgrade
 # install git, wget, curl, openssl and software-properties-common
-sudo apt install screen wget openssl redis-server supervisor nginx mysql-server php7.3 php7.3-fpm php7.3-mysql php7.3-mbstring php7.3-imagick php7.3-xml php7.3-bcmath php7.3-intl php7.3-zip -y
+sudo apt install screen wget openssl redis-server supervisor nginx mysql-server -y
 sudo mysql_secure_installation -y
 # generate three high-difficulty passwords
 PASS_WALLET=$(openssl rand 60 | openssl base64 -A)
@@ -22,6 +22,7 @@ GRANT ALL ON piratepay.* TO 'piratepay'@'localhost' IDENTIFIED BY '$PASS_MYSQL';
 FLUSH PRIVILEGES;
 QUIT;
 EOF
+sudo apt install php7.3 php7.3-fpm php7.3-mysql php7.3-mbstring php7.3-imagick php7.3-xml php7.3-bcmath php7.3-intl php7.3-zip -y
 # setting fix
 sudo sed -i /etc/php/7.3/fpm/php.ini 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g'
 # add php to system boot
