64d645c39cf6539f107080a2034d3a65a67aa754
user <esy_win@hotmail.com>
2022-03-17 15:28:53 +0100

    fix

--- install.sh
+++ install.sh
@@ -9,18 +9,17 @@ sudo apt install screen wget openssl redis-server supervisor nginx mysql-server 
 PASS_WALLET=$(openssl rand 60 | openssl base64 -A)
 PASS_MYSQL=$(openssl rand 60 | openssl base64 -A)
 PASS_REDIS=$(openssl rand 60 | openssl base64 -A)
-# pass answers to prompts
-{ 
-  echo '$PASS_MYSQL';
-  echo '$PASS_MYSQL';
-  echo 'Y';
-  echo '1';
-  echo 'Y';
-  echo 'Y';
-  echo 'Y';
-  echo 'Y';
-  echo 'Y';
-}  | sudo mysql_secure_installation
+# Make sure that NOBODY can access the server without a password
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('$PASS_MYSQL') WHERE User = 'root'"
+# Kill the anonymous users
+mysql -e "DROP USER ''@'localhost'"
+# Because our hostname varies we'll use some Bash magic here.
+mysql -e "DROP USER ''@'$(hostname)'"
+# Kill off the demo database
+mysql -e "DROP DATABASE test"
+# Make our changes take effect
+mysql -e "FLUSH PRIVILEGES"
+# Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
 
 # replace rpcpassword & rpcuser
 sed -i "2i rpcpassword=$PASS_WALLET" /home/$USER/.komodo/PIRATE/PIRATE.conf
